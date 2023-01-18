0ba6a99ba0407988aabaa59558624485622b471e
Artur <arturbr@gmail.com>
2023-01-18 00:57:53 -0300

    Toggle cache on query chain methods

--- services/cache.js
+++ services/cache.js
@@ -4,10 +4,21 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = utils.promisify(client.get);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+client.flushall('ASYNC', console.log);
+mongoose.Query.prototype.cache = function() {
+  this.useCache = true;
+
+  return this;
+}
+
 mongoose.Query.prototype.exec = async function(){
+  if(!this.useCache){
+    return exec.apply(this, arguments);
+  }
+
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name
@@ -19,14 +30,15 @@ mongoose.Query.prototype.exec = async function(){
   if(cacheValue){
     const doc = JSON.parse(cacheValue);
 
-    if(Array.isArray(doc))
+    if(Array.isArray(doc)){
       return doc.map(d => new this.model(d));
+    }
 
     return new this.model(doc);
   }
   
   // query db if cache miss
-  const result = exec.apply(this, arguments);
+  const result = await exec.apply(this, arguments);
   client.set(key, JSON.stringify(result));
 
   return result;
